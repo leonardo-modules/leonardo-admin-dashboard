2583325dde64268efe8c327979507cf311742717
Michael Kutý <6du1ro.n@gmail.com>
2016-05-16 23:32:40 +0200

    Fix dummy stuff.

--- leonardo_admin_dashboard/dashboard_modules/dummy.py
+++ leonardo_admin_dashboard/dashboard_modules/dummy.py
@@ -1,22 +1,23 @@
 # encoding: utf-8
 
 from jet.dashboard.modules import DashboardModule
+from django.utils.translation import ugettext_lazy as _
 
 
 class DummyChartWidget(DashboardModule):
     module = None
-    title = u'Počet zákazníků za 24h'
+    title = _('Visitors totals chart (dummy)')
 
     template = 'dashboard/modules/dummy_visitors_chart.html'
 
 
 class DummyVisitorsWidget(DashboardModule):
     module = None
-    title = u'Počet zákazníků'
+    title = _('Visitors totals (dummy)')
     template = 'dashboard/modules/dummy_visitors.html'
 
 
 class DummyOnlineWidget(DashboardModule):
     module = None
-    title = 'Počet zákazníků online'
+    title = _('Visitors totals count (dummy)')
     template = 'dashboard/modules/dummy_visitors_count.html'
